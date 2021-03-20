63b405a2284d1d4813d3a2fb4e799000571598a4
riki-develop <fabulous.riki@gmail.com>
2021-03-20 16:22:02 +0900

    投稿削除機能の実装

--- index.php
+++ index.php
@@ -85,8 +85,11 @@ if (isset($_REQUEST['res'])) {
           返信元のメッセージ</a>
         <?php endif; ?>
 
-          [<a href="delete.php?id="
+        <?php if ($_SESSION['id'] == $post['member_id']): ?>
+          [<a href="delete.php?id=<?php print(htmlspecialchars($post['id'])); ?>"
           style="color: #F33;">削除</a>]
+        <?php endif; ?>
+
         </p>
       </div>
     <?php endforeach; ?>
